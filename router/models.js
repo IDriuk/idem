4dbb2a6d326439584a3b11c986e3bfb4d1dc38a0
IDriuk <IDriuk@users.noreply.github.com>
2017-09-17 21:34:21 +0300

    Update models.js

--- router/models.js
+++ router/models.js
@@ -27,42 +27,5 @@ const Invite = mongoose.model('Invite', {
 
 module.exports = {
   User,
-  Invite,
-  addInvite
+  Invite
 };
-
-function addInvite(invite, user) {
-  const newInvite = new Invite(invite);
-  newInvite.save();
-
-  const newUser = new User(user);
-  newUser.save();
-
-  // {
-  //   url: 'table_tennis',
-  //   name: 'настольный теннис',
-  //   description: 'Телефон администратора (+380664358333). Когда-то тут играл с прикольным дедушкой. Лучше брать напарника с собой, на месте можно не найти.',
-  //   time: '#Время: надо звонить, могут не быть на месте',
-  //   place: '#Место: бульвар Маршала Конєва, 2',
-  //   place_gmap: 'https://goo.gl/maps/2xcLYiBGUGF2',
-  //   price: '#Цена: 50-60грн. за стол',
-  //   owner: "314236613",
-  //   position: 5,
-  //   subscribe: [],
-  //   unsubscribe: [],
-  //   archive: []
-  // }
-
-  // {
-  //   uid: "314236613",
-  //   first_name: 'Виктор',
-  //   photo_rec: "https://pp.userapi.com/c627221/v627221613/1ab48/Jef9P48vVf8.jpg"
-  // }
-
-  // Invite.update({owner: "12949688"}, { place_gmap: "https://goo.gl/maps/DuAD1AJwWEE2" }, { multi: true }, function (err, raw) {
-  //   console.log('The raw response from Mongo was ', raw);
-  // });
-  // Invite.update({}, { position: 1 }, function (err, raw) {
-  //   console.log('The raw response from Mongo was ', raw);
-  // });
-}
